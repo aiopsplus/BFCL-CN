aiopsplus/BFCL-CN

commit 53180782d2bead1b35bd1b428fce7303e21f36b0

Leaderboard Update April 1 (#299)
This PR is for the Leaderboard April 1 update. 
This update comes with new models (`Claude-3-Haiku`,
`Databrick-DBRX-Instruct`), more advanced AST evaluation process, and
updated evaluation datasets. Cost and latency statistics during
evaluation are also measured. We also released the manual that our
evaluation is based on.

Does this affect leaderboard score? 
Yes! Read updated blog 8 - leaderboard to learn more!




---------

Co-authored-by: Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
Co-authored-by: Fanjia Yan <fanjiayan@berkeley.edu>

model_handler/mistral_handler.py
@@ -1,0 +1,122 @@
+from model_handler.handler import BaseHandler
+from model_handler.model_style import ModelStyle
+from model_handler.constant import (
+    SYSTEM_PROMPT_FOR_CHAT_MODEL,
+    USER_PROMPT_FOR_CHAT_MODEL,
+    GORILLA_TO_OPENAPI,
+)
+from model_handler.utils import (
+    convert_to_tool,
+    ast_parse,
+    convert_to_function_call,
+    augment_prompt_by_languge,
+    language_specific_pre_processing,
+)
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+import os, time, json
+
+
+class MistralHandler(BaseHandler):
+    def __init__(self, model_name, temperature=0.7, top_p=1, max_tokens=1000) -> None:
+        super().__init__(model_name, temperature, top_p, max_tokens)
+        self.model_style = ModelStyle.Mistral
+
+        self.client = MistralClient(api_key=os.getenv("MISTRAL_API_KEY"))
+
+    def inference(self, prompt, functions, test_category):
+        prompt = augment_prompt_by_languge(prompt, test_category)
+        if "FC" in self.model_name:
+            functions = language_specific_pre_processing(functions, test_category, True)
+            tool = convert_to_tool(
+                functions, GORILLA_TO_OPENAPI, self.model_style, test_category, True
+            )
+            message = [
+                ChatMessage(role="user", content=prompt),
+            ]
+            start = time.time()
+            if "Any" in self.model_name:
+                tool_choice = "any"
+            else:
+                tool_choice = "auto"
+            chat_response = self.client.chat(
+                model=self.model_name.replace("-FC-Any", "").replace("-FC-Auto", ""),
+                messages=message,
+                tools=tool,
+                tool_choice=tool_choice,
+                temperature=self.temperature,
+                top_p=self.top_p,
+            )
+            latency = time.time() - start
+            try:
+                result = [
+                    {func_call.function.name: func_call.function.arguments}
+                    for func_call in chat_response.choices[0].message.tool_calls
+                ]
+            except:
+                result = chat_response.choices[0].message.content
+        else:
+            functions = language_specific_pre_processing(
+                functions, test_category, False
+            )
+            message = [
+                ChatMessage(role="system", content=SYSTEM_PROMPT_FOR_CHAT_MODEL),
+                ChatMessage(
+                    role="user",
+                    content=USER_PROMPT_FOR_CHAT_MODEL.format(
+                        user_prompt=prompt, functions=str(functions)
+                    ),
+                ),
+            ]
+            start = time.time()
+            chat_response = self.client.chat(
+                model=self.model_name,
+                messages=message,
+                temperature=self.temperature,
+                top_p=self.top_p,
+            )
+            latency = time.time() - start
+            result = chat_response.choices[0].message.content
+        metadata = {
+            "input_tokens": chat_response.usage.prompt_tokens,
+            "output_tokens": chat_response.usage.completion_tokens,
+            "latency": latency,
+        }
+        return result, metadata
+
+    def decode_ast(self, result, language="Python"):
+        if "FC" in self.model_name:
+            decoded_output = []
+            for invoked_function in result:
+                name = list(invoked_function.keys())[0]
+                params = json.loads(invoked_function[name])
+                if language != "Python":
+                    for key in params:
+                        params[key] = str(params[key])
+                decoded_output.append({name: params})
+            return decoded_output
+        else:
+            func = result.replace('", "', ",").replace('["', "[").replace('"]', "]")
+            func = func.replace("\\_", "_")
+            if not func.startswith("["):
+                func = "[" + func
+            if not func.endswith("]"):
+                func = func + "]"
+            decoded_output = ast_parse(func, language)
+            return decoded_output
+
+    def decode_execute(self, result):
+        if "FC" in self.model_name:
+            function_call = convert_to_function_call(result)
+            return function_call
+        else:
+            func = result
+            func = func.replace("\\_", "_")
+            decode_output = ast_parse(func)
+            execution_list = []
+            for function_call in decode_output:
+                for key, value in function_call.items():
+                    execution_list.append(
+                        f"{key}({','.join([f'{k}={repr(v)}' for k, v in value.items()])})"
+                    )
+            return execution_list

model_handler/oss_handler.py
@@ -1,0 +1,142 @@
+from model_handler.handler import BaseHandler
+from model_handler.model_style import ModelStyle
+from model_handler.constant import MODEL_ID_DICT
+from model_handler.utils import (
+    ast_parse,
+    augment_prompt_by_languge,
+    language_specific_pre_processing,
+)
+from tqdm import tqdm
+import shortuuid, ray, os, json, torch
+
+
+class OSSHandler(BaseHandler):
+    def __init__(self, model_name, temperature=0.7, top_p=1, max_tokens=1000) -> None:
+        super().__init__(model_name, temperature, top_p, max_tokens)
+        self.model_style = ModelStyle.OSSMODEL
+        self._init_model()
+
+    def _init_model(self):
+        ray.init(ignore_reinit_error=True, num_cpus=8)
+
+    def _format_prompt(prompt, function):
+        SYSTEM_PROMPT = """
+            You are an helpful assistant who has access to the following functions to help the user, you can use the functions if needed-
+        """
+        functions = ""
+        if isinstance(function, list):
+            for idx, func in enumerate(function):
+                functions += "\n" + str(func)
+        else:
+            functions += "\n" + str(function)
+        return f"SYSTEM: {SYSTEM_PROMPT}\n{functions}\nUSER: {prompt}\nASSISTANT: "
+
+    @ray.remote(num_gpus=1)
+    @torch.inference_mode()
+    def _batch_generate(
+        question_jsons,
+        test_category,
+        model_path,
+        temperature,
+        max_tokens,
+        top_p,
+        format_prompt_func,
+    ):
+        from vllm import LLM, SamplingParams
+
+        prompts = []
+        ans_jsons = []
+        for line in question_jsons:
+            ques_json = line
+            prompt = augment_prompt_by_languge(ques_json["question"], test_category)
+            functions = language_specific_pre_processing(
+                ques_json["function"], test_category, False
+            )
+            prompts.append(format_prompt_func(prompt, functions, test_category))
+            ans_id = shortuuid.uuid()
+            ans_jsons.append(
+                {
+                    "answer_id": ans_id,
+                    "question": ques_json["question"],
+                }
+            )
+
+        print("start generating: ", len(prompts))
+        sampling_params = SamplingParams(
+            temperature=temperature, max_tokens=max_tokens, top_p=top_p
+        )
+        llm = LLM(model=model_path, dtype="float16", trust_remote_code=True)
+        outputs = llm.generate(prompts, sampling_params)
+        final_ans_jsons = []
+        for output, ans_json in zip(outputs, ans_jsons):
+            text = output.outputs[0].text
+            ans_json["text"] = text
+            final_ans_jsons.append(ans_json)
+        return final_ans_jsons
+
+    def inference(
+        self, question_file, test_category, num_gpus, format_prompt_func=_format_prompt
+    ):
+
+        ques_jsons = []
+        with open(question_file, "r") as ques_file:
+            for line in ques_file:
+                ques_jsons.append(json.loads(line))
+
+        chunk_size = len(ques_jsons) // num_gpus
+        ans_handles = []
+        for i in range(0, len(ques_jsons), chunk_size):
+            ans_handles.append(
+                self._batch_generate.remote(
+                    ques_jsons[i : i + chunk_size],
+                    test_category,
+                    self.model_name,
+                    self.temperature,
+                    self.max_tokens,
+                    self.top_p,
+                    format_prompt_func,
+                )
+            )
+        ans_jsons = []
+        for ans_handle in ans_handles:
+            ans_jsons.extend(ray.get(ans_handle))
+
+        return ans_jsons, {"input_tokens": 0, "output_tokens": 0, "latency": 0}
+
+    def decode_ast(self, result, language="Python"):
+        func = result.replace('", "', ",").replace('["', "[").replace('"]', "]")
+        if " " == func[0]:
+            func = func[1:]
+        if not func.startswith("["):
+            func = "[" + func
+        if not func.endswith("]"):
+            func = func + "]"
+        decode_output = ast_parse(func, language)
+        return decode_output
+
+    def decode_execute(self, result):
+        return result
+
+    def write(self, result, file_to_open):
+        if not os.path.exists("./result"):
+            os.mkdir("./result")
+        if not os.path.exists("./result/" + self.model_name.replace("/", "_")):
+            os.mkdir("./result/" + self.model_name.replace("/", "_"))
+        with open(
+            "./result/" + self.model_name.replace("/", "_") + "/" + file_to_open, "a+"
+        ) as f:
+            f.write(json.dumps(result) + "\n")
+
+    def load_result(self, test_category):
+        eval_data = []
+        with open("./eval_data_total.json") as f:
+            for line in f:
+                eval_data.append(json.loads(line))
+        result_list = []
+        idx = 0
+        with open(f"./result/{self.model_name}/result.json") as f:
+            for line in f:
+                if eval_data[idx]["test_category"] == test_category:
+                    result_list.append(json.loads(line))
+                idx += 1
+        return result_list

commit 19baa9ace43e952d589ecb08540e88ab1dc62fab

BFCL April 8th Release (#330)
This PR is for the leaderboard April 8th release:

1. Fixed an oversight that was introduced in #299. For function-calling
(FC) models that cannot take `float` type in input, when the parameter
type is a `float`, the evaluation procedure will convert that type to
`number` in the model input and mention in the parameter description
that `This is a float type value.`. An additional field `format: float`
will also be included in the model input to make it clear about the
type.
2. Update the model handler for Claude, Mistral, and OSS to better parse
the model output. This is to patch the handler we released in #299, as
it sometimes fails to parse even though the model output is valid. This
affects only the prompting models; the FC models are unaffected.


This PR **DOES** change the leaderboard score. We will update the
leaderboard website shortly, in a different PR.

---------

Co-authored-by: Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
Co-authored-by: Fanjia Yan <fanjiayan@berkeley.edu>

model_handler/mistral_handler.py
@@ -96,7 +96,7 @@ class MistralHandler(BaseHandler):
                 decoded_output.append({name: params})
             return decoded_output
         else:
-            func = result.replace('", "', ",").replace('["', "[").replace('"]', "]")
+            func = result
             func = func.replace("\\_", "_")
             if not func.startswith("["):
                 func = "[" + func

model_handler/oss_handler.py
@@ -104,7 +104,7 @@ class OSSHandler(BaseHandler):
         return ans_jsons, {"input_tokens": 0, "output_tokens": 0, "latency": 0}
 
     def decode_ast(self, result, language="Python"):
-        func = result.replace('", "', ",").replace('["', "[").replace('"]', "]")
+        func = result
         if " " == func[0]:
             func = func[1:]
         if not func.startswith("["):

model_handler/utils.py
@@ -3,6 +3,7 @@ from model_handler.model_style import ModelStyle
 from model_handler.constant import JAVA_TYPE_CONVERSION, JS_TYPE_CONVERSION
 from model_handler.java_parser import parse_java_function_call
 from model_handler.js_parser import parse_javascript_function_call
+from model_handler.constant import GORILLA_TO_OPENAPI
 
 
 def _cast_to_openai_type(properties, mapping, test_category):
@@ -11,6 +12,9 @@ def _cast_to_openai_type(properties, mapping, test_category):
             properties[key]["type"] = "string"
         else:
             var_type = value["type"]
+            if mapping == GORILLA_TO_OPENAPI and var_type == "float":
+                properties[key]["format"] = "float"
+                properties[key]["description"] += " This is a float type value."
             if var_type in mapping:
                 properties[key]["type"] = mapping[var_type]
             else:
@@ -233,7 +237,10 @@ def resolve_ast_by_type(value):
     elif isinstance(value, ast.Ellipsis):
         output = "..."
     elif isinstance(value, ast.Subscript):
-        output = ast.unparse(value.body[0].value)
+        try:
+            output = ast.unparse(value.body[0].value)
+        except:
+            output = ast.unparse(value.value) + "[" + ast.unparse(value.slice) + "]"
     else:
         raise Exception(f"Unsupported AST type: {type(value)}")
     return output